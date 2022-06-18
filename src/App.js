c5b9764d0dafbc3ea7d97f24ade965d978f126ab
NathanDBrowne <nathan.browne1@gmail.com>
2022-06-18 18:34:58 +0100

    revert switch change

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from "./components/pages/Home";
 import Products from "./components/pages/Products";
 import Services from "./components/pages/Services";
 import SignUp from "./components/pages/SignUp";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
 function App() {
@@ -14,12 +14,12 @@ function App() {
     <>
       <Router>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/services" element={<Services />} />
           <Route path="/sign-up" element={<SignUp />} />
-        </Switch>
+        </Routes>
         <Footer />
       </Router>
     </>
